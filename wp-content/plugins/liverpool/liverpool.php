f27477d6893b726f0761120f52743391ed40b0a5
jduval <jero.duval@gmail.com>
2013-06-18 12:19:55 +0200

    picture upload feature done

--- wp-content/plugins/liverpool/liverpool.php
+++ wp-content/plugins/liverpool/liverpool.php
@@ -9,15 +9,6 @@ Author URI: http://dominiquepierre.fr/
 License: GNULIGAN
 */
 
-add_action('init', 'liverpool_init');
-add_action('add_meta_boxes', 'liverpool_info_meta_box');
-add_action('add_meta_boxes', 'liverpool_img_meta_box'); // upload img
-
-add_action('save_post', 'liverpool_meta_box_save');
-
-add_action('manage_posts_custom_column', 'liverpool_manage_posts_custom_column', 10, 2);
-add_filter('manage_edit-player_columns', 'liverpool_manage_posts_columns');
-
 function liverpool_init ()
 {
 
@@ -47,6 +38,7 @@ function liverpool_init ()
   );
 
 } // end liverpool_init();
+add_action('init', 'liverpool_init');
 
 // adding player's info meta box
 function liverpool_info_meta_box()
@@ -61,6 +53,7 @@ function liverpool_info_meta_box()
     'high'
   );
 }
+add_action('add_meta_boxes', 'liverpool_info_meta_box');
 
 // display of meta box with values
 function liverpool_meta_box_cb($post)
@@ -74,6 +67,7 @@ function liverpool_meta_box_cb($post)
   $lastname   = isset( $values['meta_box_lastname'] ) ? $values['meta_box_lastname'][0] : null;
   $position   = isset( $values['meta_box_position'] ) ? $values['meta_box_position'][0] : null;
   $number     = isset( $values['meta_box_number'] ) ? $values['meta_box_number'][0] : null;
+  $image      = isset( $values['wp_custom_image'] ) ? $values['wp_custom_image']['url'] : null;
 
   //The nonce field is used to validate that the contents of the form request came from the current site and not somewhere else.
   wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
@@ -107,6 +101,10 @@ function liverpool_meta_box_cb($post)
               ?>
             </select>
         </p>
+        <p>
+          <p>Current picture : <?php echo isset ( $image ) ? $image : 'None' ; ?></p>
+        </p>
+
 <?php
 } // liverpool meta box cb function end
 
@@ -124,10 +122,11 @@ function liverpool_img_meta_box() {
   );
 
 } // end liverpool_img_meta_box
+add_action('add_meta_boxes', 'liverpool_img_meta_box'); // upload img
 
 function wp_img_attachment() {
 
-  //wp_nonce_field(plugin_basename(__FILE__), 'wp_custom_image_nonce');
+  wp_nonce_field(plugin_basename(__FILE__), 'wp_custom_image_nonce');
 
   $html = '<p class="description">';
   $html .= 'Upload player\'s picture here.';
@@ -138,31 +137,15 @@ function wp_img_attachment() {
 
 } // end wp_img_attachment
 
-//function save_custom_meta_data($id) {
+function modify_form() // allows form to send $_FILE
+{
 
-  /* --- security verification --- */
-  //if(!wp_verify_nonce($_POST['wp_custom_attachment_nonce'], plugin_basename(__FILE__))) {
-    //return $id;
-  //} // end if
+  echo '<script type="text/javascript">
+          jQuery("#post").attr("enctype", "multipart/form-data");
+        </script>';
 
-  //if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-    //return $id;
-  //} // end if
-
-  //if('page' == $_POST['post_type']) {
-    //if(!current_user_can('edit_page', $id)) {
-      //return $id;
-    //} // end if
-  //} else {
-    //if(!current_user_can('edit_page', $id)) {
-      //return $id;
-    //} // end if
-  //} // end if
-  /* - end security verification - */
-
-//} // end save_custom_meta_data
-//add_action('save_post', 'save_custom_meta_data');
-
+}
+add_action('admin_footer','modify_form');
 
 // save edit or new player
 function liverpool_meta_box_save( $post_id )
@@ -178,8 +161,6 @@ function liverpool_meta_box_save( $post_id )
   if( !current_user_can( 'edit_post' ) )
     return;
 
-  var_dump($_POST);
-
   if( isset( $_POST['meta_box_firstname'] ) )
     update_post_meta( $post_id, 'meta_box_firstname', wp_kses_post( $_POST['meta_box_firstname']) );
 
@@ -192,82 +173,83 @@ function liverpool_meta_box_save( $post_id )
   if( isset( $_POST['meta_box_number'] ) )
     update_post_meta( $post_id, 'meta_box_number', esc_attr( $_POST['meta_box_number'] ) );
 
-var_dump($_FILES);
-
   // Make sure the file array isn't empty
-  if(!empty($_FILES['wp_custom_attachment']['name'])) {
+  if(!empty($_FILES['wp_custom_image']['name'])) {
 
     // Setup the array of supported file types. In this case, it's just PDF.
     $supported_types = array('image/png','image/jpeg');
 
     // Get the file type of the upload
-    $arr_file_type = wp_check_filetype(basename($_FILES['wp_custom_attachment']['name']));
+    $arr_file_type = wp_check_filetype(basename($_FILES['wp_custom_image']['name']));
     $uploaded_type = $arr_file_type['type'];
 
     // Check if the type is supported. If not, throw an error.
     if(in_array($uploaded_type, $supported_types)) {
 
       // Use the WordPress API to upload the file
-      $upload = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name']));
+      $upload = wp_upload_bits($_FILES['wp_custom_image']['name'], null, file_get_contents($_FILES['wp_custom_image']['tmp_name']));
 
       if(isset($upload['error']) && $upload['error'] != 0) {
         wp_die('There was an error uploading your file. The error is: ' . $upload['error']);
       } else {
-        add_post_meta($post_id, 'wp_custom_attachment', $upload);
-        update_post_meta($post_id, 'wp_custom_attachment', $upload);
+        update_post_meta($post_id, 'wp_custom_image', $upload);
       } // end if/else
 
     } else {
-      wp_die("The file type that you've uploaded is not a PDF.");
+      wp_die("The file type that you've uploaded is not a image.");
     } // end if/else
 
   } // end if
-  else
-    echo 'FAIL NOOB';exit;
 
 } // end save
+add_action('save_post', 'liverpool_meta_box_save');
 
 // frontend table display
 function liverpool_show($limit = 5)
 {
+
   $players = new WP_Query("post_type=player&posts_per_pasge=$limit");
 
   ?>
-    <div id="liverpool-players">
-      <table class="players-table">
-        <thead>
-          <tr>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Position</th>
-            <th>Number</th>
-          </tr>
-        </thead>
-        <tbody>
-        <?php
-          if ( $players->have_posts() ) :
-            while ( $players->have_posts() ) : $players->the_post();
-              global $post;
-              echo ("
-                <tr>
-                  <td>" . get_post_meta($post->ID,'meta_box_firstname',true) . "</td>
-                  <td>" . get_post_meta($post->ID,'meta_box_lastname',true) . "</td>
-                  <td>" . get_post_meta($post->ID,'meta_box_position',true) . "</td>
-                  <td>" . get_post_meta($post->ID,'meta_box_number',true) . "</td>
-                </tr>
-              ");
-            endwhile;
-          else :
-            echo wpautop( 'Sorry, no players were found' );
-          endif;
-        ?>
-        </tbody>
-      </table>
-    </div>
+  <div id="liverpool-players">
+    <table class="players-table">
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Position</th>
+          <th>Number</th>
+          <th>Picture</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+        if ( $players->have_posts() ) :
+          while ( $players->have_posts() ) : $players->the_post();
+            global $post;
+            $image = get_post_meta($post->ID,'wp_custom_image', true);
+            if ( ! ( isset ( $image ) && $image ) )
+              $image['url'] = null;
+            echo ("
+              <tr>
+                <td>" . get_post_meta($post->ID, 'meta_box_firstname', true) . "</td>
+                <td>" . get_post_meta($post->ID, 'meta_box_lastname', true) . "</td>
+                <td>" . get_post_meta($post->ID, 'meta_box_position', true) . "</td>
+                <td>" . get_post_meta($post->ID, 'meta_box_number', true) . "</td>
+                <td><img src='{$image['url']}' width=150 height=150 />
+              </tr>
+            ");
+          endwhile;
+        else :
+          echo wpautop( 'Sorry, no players were found' );
+        endif;
+      ?>
+      </tbody>
+    </table>
+  </div>
 
 <?php
 } // end liverpool_show function
-
 // write [liverpool_crud] to display the crud
 add_shortcode('liverpool_crud','liverpool_show');
 
@@ -281,13 +263,16 @@ function liverpool_manage_posts_columns($columns)
       'lastname'        => __('Last name'),
       'team'            => __('Position'),
       'number'          => __('Number'),
+      'picture'         => __('Picture'),
       'edit'            => __('Edit')
     ));
   return $columns;
 }
+add_filter('manage_edit-player_columns', 'liverpool_manage_posts_columns');
 
 function liverpool_manage_posts_custom_column($column, $post_id)
 {
+
   switch ($column) {
     case 'firstname':
       $liverpool_player = get_post_meta($post_id, 'meta_box_firstname', true);
@@ -299,12 +284,20 @@ function liverpool_manage_posts_custom_column($column, $post_id)
       $liverpool_player = get_post_meta($post_id, 'meta_box_position', true);
       break;
     case 'number':
-       $liverpool_player = get_post_meta($post_id, 'meta_box_number', true);
-       break;
+      $liverpool_player = get_post_meta($post_id, 'meta_box_number', true);
+      break;
+    case 'picture':
+      $image = get_post_meta($post_id, 'wp_custom_image', true);
+      if ( isset ( $image ) && $image ) {
+        $liverpool_player = "<img src='{$image['url']}' width=150 height=150 />";
+      }
+      break;
     case 'edit':
-       $liverpool_player = "<a href='post.php?post={$post_id}&action=edit'>Edit</a>";
-       break;
+      $liverpool_player = "<a href='post.php?post={$post_id}&action=edit'>Edit</a>";
+      break;
   }
 
   echo $liverpool_player;
+
 }
+add_action('manage_posts_custom_column', 'liverpool_manage_posts_custom_column', 10, 2);
